5da00ef600df7de41ecdf95c1b03490a72e03116
sutepjantawee <bananammm0001@gmail.com>
2023-02-06 23:50:08 +0700

    add tailwind and input

--- src/component/Home.js
+++ src/component/Home.js
@@ -11,17 +11,18 @@ function Home() {
 
   const dispatch = useDispatch();
 
-  const APIKey = "aafcf985";
+  // const APIKey = "aafcf985";
   // axios.create({
   //   baseURL: "https://www.omdbapi.com/"
   // });
-
+  const [search, setSearch] = useState("");
   useEffect(() => {
     UsersGet()
-  }, [])
+  },[search])
   
 
   const UsersGet = () => {
+    // event.preventDefault();
     // //ดึงapi
     // fetch("https://www.melivecode.com/api/users")
     // .then(res => res.json())
@@ -35,12 +36,12 @@ function Home() {
     //     console.log(error);
     //   }
     // )
-
+  
     const fetchMovies = async () => {
-      // const searchKey = search ? search : "Thor";
+      const searchKey = search ? search : "batman";
       const resp = await  axios.create({
         baseURL: "https://www.omdbapi.com/"
-      }).get(`?apikey=aafcf985&s=batman&type=movie`)
+      }).get(`?apikey=aafcf985&s=${searchKey}&type=movie`)
                                  .catch((error) => {
                                       console.log(error)
                                  })
@@ -57,13 +58,39 @@ function Home() {
 
   return (
     <div>
-      HomeHomeHome
+      <div className="text-5xl py-5">
+      movies
+      </div>
+      
       <div className="">
+        <div className="flex w-full justify-center">
+        <div className="pt-1 pr-3 text-xl">Name : </div>
+        <div className="">
+          <input
+           onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          name="price"
+          id="price"
+          className=" h-10 rounded-md bg-slate-700  border-gray-500 pl-7 pr-12 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+          placeholder="Search..."
+        />
+      </div>
+        </div>
+       
+         
          { data && data.map((data) =>(
-          <div key={data.imdbID}>
-            <div>
-            <img src={data.Poster} alt=""/>
+          <div key={data.imdbID} >
+            <div className="grid justify-items-center py-5 ">
+              <div className="rounded-full">
+              <img src={data.Poster} alt="" className="rounded-lg w-32" />
+              </div>
+              <div className="">
             {data.Title}
+            </div>
+            <div className="">
+            Year : {data.Year}
+            </div>
+           
             </div>
             
           </div>
